77f4202e7c6d767e82551081342511ef8ae90205
Cole Stewart <col.j.stew@gmail.com>
2022-09-19 09:46:55 -0600

    Updated DTO

--- Models/TodoItemsDTO.cs
+++ Models/TodoItemsDTO.cs
@@ -3,7 +3,9 @@ namespace TodoApi.Models
     public class TodoItemDTO
     {
         public long Id { get; set; }
-        public string? Name { get; set; }
+        public string Name { get; set; } = null!;
+        public string? Detail { get; set; }
+        public Priority Priority { get; set; }
         public bool IsComplete { get; set; }
     }
 }
